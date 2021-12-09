1d67cce492182b6f07742ea33255e52d02b5d94c
Sahishnav Pillai <50857800+Nav2k19@users.noreply.github.com>
2021-12-09 15:46:25 -0600

    Just set up express server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+//express variable gives access to express library
+const express = require('express');
+//variable app is the declared instance as the constructor 
+const app = express();
+
+app.listen(8000,function(){
+    console.log("server is running")
+})
+
+
+
+

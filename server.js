a66a04f7d97af69c8103cf9a6f400561bffdd58b
Sahishnav Pillai <50857800+Nav2k19@users.noreply.github.com>
2021-12-09 15:51:10 -0600

    Added first GET route

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const express = require('express');
 //variable app is the declared instance as the constructor 
 const app = express();
 
+
+const mockUsers = [
+    {name: "OG"},
+    {name: "Bam"}
+]
+
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message: 'successsfully retrieved users',
+        users: mockUsers
+
+    })
+})
+
 app.listen(8000,function(){
     console.log("server is running")
 })
